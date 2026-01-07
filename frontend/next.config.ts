3cca92f95840d4b0df2f31a0fb3f3b870a94d582
Siyad-S <siyadsaidu313@gmail.com>
2026-01-07 23:33:08 +0530

    redirection to home has fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -26,6 +26,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
